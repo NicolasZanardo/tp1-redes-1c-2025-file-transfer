7e6dec824f54c268b0f74ccd1c8fde24407d92b8
Alexander Villa <avilla@fi.uba.ar>
2025-04-20 15:54:36 -0300

    se agrega parametro help

--- src/download.py
+++ src/download.py
@@ -2,10 +2,10 @@ from librerias.client import Client
 import argparse
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Upload files to a server.")
+    parser = argparse.ArgumentParser(description="Download files from server.")
 
     # Argumentos opcionales
-    #se debe poner el de help -h???
+    parser.add_argument('-h', '--help', help='show this help message and exit')
     parser.add_argument('-v', '--verbose', action='store_true', help="Increase output verbosity")
     parser.add_argument('-q', '--quiet', action='store_true', help="Decrease output verbosity")
     parser.add_argument('-H', '--host', type=str, default="localhost", help="Server IP address")
--- src/start-server.py
+++ src/start-server.py
@@ -5,6 +5,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Upload files to a server.")
 
     # Argumentos opcionales
+    parser.add_argument('-h', '--help', help='show this help message and exit')
     parser.add_argument('-v', '--verbose', action='store_true', help="Increase output verbosity")
     parser.add_argument('-q', '--quiet', action='store_true', help="Decrease output verbosity")
     parser.add_argument('-H', '--host', type=str, default="127.0.0.1", help="Server IP address")
