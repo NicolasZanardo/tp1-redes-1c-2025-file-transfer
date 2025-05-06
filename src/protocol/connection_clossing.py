1613c5efe885e4f13c390922f2334413ed26d1e2
mmedela <65471451+mmedela@users.noreply.github.com>
2025-05-05 21:10:29 -0300

    fix sr

--- src/protocol/connection_clossing.py
+++ src/protocol/connection_clossing.py
@@ -8,10 +8,14 @@ class ConnectionClossing:
             socket.sendto(b'CLOSE', peer_address)
             Logger.debug(who=socket.getsockname(), message="Sent CLOSE message")
             socket.settimeout(1.0)
-            data, addr = socket.recvfrom(1024)
+            data, _ = socket.recvfrom(1024)
             if data == b'CLOSE_ACK':
                 Logger.debug(who=socket.getsockname(), message="Received CLOSE_ACK")
-                return True
+                data2, _ = socket.recvfrom(1024)
+                if data2 == b'FIN':
+                    socket.sendto(b'FIN_ACK', peer_address)
+                    socket.settimeout(30.0)
+                    return True
             else:
                 Logger.error("Failed to receive CLOSE_ACK")
                 return False
@@ -23,9 +27,21 @@ class ConnectionClossing:
     def respond_to_clossing(cls, socket, addr):
         """Respond to a CLOSE message by sending CLOSE_ACK."""
         try:
+            data, addr = socket.recvfrom(1024)
+            if data != b'CLOSE':
+                raise Exception("Error receiving CLOSE: Received {data}")
             Logger.debug(who=socket.getsockname(), message="Received CLOSE, sending CLOSE_ACK")
             socket.sendto(b'CLOSE_ACK', addr)
-            return True
+            socket.settimeout(1.0)
+            socket.sendto(b'FIN', addr)
+            socket.settimeout(2.0)
+            data, addr = socket.recvfrom(1024)
+            Logger.debug(message=f"Received {data}")
+            if data == b'FIN_ACK':
+                return True
+            else:
+                raise Exception("Error recieving FIN_ACK")
+            
         except Exception as e:
             Logger.error(f"Error in respond_to_clossing: {e}")
             return False
